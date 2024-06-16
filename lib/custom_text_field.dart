091f59ef63866e8e0f463f4d40d3e12b885af303
michael-k <michaelsure9@gmail.com>
2024-06-16 14:02:09 +0300

    refactoring

--- lib/custom_text_field.dart
+++ lib/custom_text_field.dart
@@ -1,47 +1,58 @@
-// lib/custom_text_field.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/text_field_bloc.dart';
 import 'bloc/text_field_event.dart';
 import 'bloc/text_field_state.dart';
+import 'callback_fun_type.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController _controller = TextEditingController();
   final FocusNode _focusNode = FocusNode();
 
-  CustomTextField({super.key});
+  late TextFieldBloc textFieldBloc;
+  final VoidCallbackParameter? callback;
+
+  CustomTextField(this.callback, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TextFieldBloc, TextFieldState>(
-      builder: (context, state) {
-        final textFieldBloc = BlocProvider.of<TextFieldBloc>(context);
-
-        if (state is TextFieldChanged) {
-          _controller.value = _controller.value.copyWith(
-            text: state.text,
-            selection: TextSelection.collapsed(offset: state.text.length),
-          );
-        }
-
-        return TextField(
-          controller: _controller,
-          focusNode: _focusNode,
-          maxLines: null,  // Makes the TextField multiline
-          onChanged: (text) {
-            // Example of formatting: capitalize every letter
-            String formattedText = text.toUpperCase();
-            textFieldBloc.add(TextChanged(formattedText));
-          },
-          onSubmitted: (text) {
-            textFieldBloc.add(TextSubmitted(text));
-          },
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: 'Enter your text here...',
-          ),
-        );
+    return BlocProvider(
+      //create: (context) => TextFieldBloc(),
+      create: (context) {
+        textFieldBloc = TextFieldBloc();
+        return textFieldBloc;
       },
+      child: BlocBuilder<TextFieldBloc, TextFieldState>(
+        builder: (context, state) {
+          //final textFieldBloc = BlocProvider.of<TextFieldBloc>(context);
+
+          if (state is TextFieldChanged) {
+            _controller.value = _controller.value.copyWith(
+              text: state.text,
+              selection: TextSelection.collapsed(offset: state.text.length),
+            );
+          }
+
+          return TextField(
+            controller: _controller,
+            focusNode: _focusNode,
+            maxLines: null,  // Makes the TextField multiline
+            onChanged: (text) {
+              // Example of formatting: capitalize every letter
+              String formattedText = text.toUpperCase();
+              textFieldBloc.add(TextChanged(formattedText));
+              callback?.call(formattedText);
+            },
+            onSubmitted: (text) {
+              textFieldBloc.add(TextSubmitted(text));
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your text here...',
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'bloc/text_field_bloc.dart';
-import 'bloc/text_field_event.dart';
-import 'bloc/text_field_state.dart';
 import 'custom_text_field.dart';
-import 'custom_text_field2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,22 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: TextFieldPage(),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     home: BlocProvider(
-  //       create: (context) => TextFieldBloc(),
-  //       child: TextFieldPage(),
-  //     ),
-  //   );
-  // }
-
-
 }
 
 class TextFieldPage extends StatelessWidget {
@@ -40,87 +23,23 @@ class TextFieldPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BLoC Custom TextField Example'),
+        title: const Text('BLoC Custom TextField Example'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             //CustomTextField(),
-            CustomTextField2((text) {
+            CustomTextField((text) {
               debugPrint('callback 1->[$text]');
             }),
 
-            CustomTextField2((text) {
+            CustomTextField((text) {
               debugPrint('callback 2->[$text]');
             }),
-
-            // BlocBuilder<TextFieldBloc, TextFieldState>(
-            //   builder: (context, state) {
-            //     String displayText = '';
-            //     if (state is TextFieldChanged) {
-            //       displayText = state.text;
-            //     }
-            //     return Padding(
-            //       padding: const EdgeInsets.only(top: 20),
-            //       child: Text('Text: $displayText'),
-            //     );
-            //   },
-            // ),
           ],
         ),
       ),
     );
   }
 }
-// class TextFieldPage extends StatelessWidget {
-//   const TextFieldPage({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final TextFieldBloc textFieldBloc = BlocProvider.of<TextFieldBloc>(context);
-//
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('BLoC TextField Example'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             BlocBuilder<TextFieldBloc, TextFieldState>(
-//               builder: (context, state) {
-//                 String displayText = '';
-//                 if (state is TextFieldChanged) {
-//                   displayText = state.text;
-//                 }
-//
-//                 return Column(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   children: [
-//                     TextField(
-//                       maxLines: null,
-//                       onChanged: (text) {
-//                         textFieldBloc.add(TextChanged(text));
-//                       },
-//                       onSubmitted: (text) {
-//                         debugPrint('submitted: $text');
-//                         textFieldBloc.add(TextSubmitted(text));
-//                       },
-//                       decoration: const InputDecoration(
-//                         border: OutlineInputBorder(),
-//                         hintText: 'Enter your text here...',
-//                       ),
-//                     ),
-//                     const SizedBox(height: 20),
-//                     Text('Text: $displayText'),
-//                   ],
-//                 );
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
